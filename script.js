0724d1f3b192ee1986e20d99155b552fc4c5b358
Garrett <gcamoin5@gmail.com>
2023-02-15 08:57:32 -0500

    added form

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const cocktailForm = document.getElementById('cocktail-form')
+    cocktailForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const cocktail = document.getElementById('search').value
+        console.log(cocktail)
+    })
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+})
