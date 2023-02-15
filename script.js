8b47ec6faa3606ad34739e3105331c6d7a7cea4a
Garrett <gcamoin5@gmail.com>
2023-02-15 12:55:31 -0500

    fetch request

--- script.js
+++ script.js
@@ -3,9 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const cocktailForm = document.getElementById('cocktail-form')
     cocktailForm.addEventListener('submit', (event) => {
         event.preventDefault();
-        const cocktail = document.getElementById('search').value
+        const cocktailName = document.getElementById('search').value
         console.log(cocktail)
+
+        fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(json => renderDrinks(json.drinks))
     })
+
+    function renderDrinks(array) {
+        array.forEach((drink) => {
+            const cocktailList = document.getElementById('cocktail-list')
+
+        })
+
+    }
 
 
 
