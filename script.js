3b0848c90458f8cae4976fd1f0e1a0af9af7f46c
Garrett <gcamoin5@gmail.com>
2023-02-16 14:21:50 -0500

    Append drinks to DOM

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
     cocktailForm.addEventListener('submit', (event) => {
         event.preventDefault();
         const cocktailName = document.getElementById('search').value
-        console.log(cocktail)
+        console.log(cocktailName)
 
-        fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
+        fetch(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
         .then(function (response) {
             return response.json();
         })
@@ -14,8 +14,11 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     function renderDrinks(array) {
-        array.forEach((drink) => {
+        array.forEach((eachDrink) => {
             const cocktailList = document.getElementById('cocktail-list')
+            const drinkNameLi = document.createElement('li')
+            drinkNameLi.innerText = eachDrink.strDrink
+            cocktailList.appendChild(drinkNameLi)
 
         })
 
