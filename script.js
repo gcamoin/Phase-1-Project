5bc4631d0f64016f0b6d004332d5b5515bfb21e3
Garrett <gcamoin5@gmail.com>
2023-02-17 08:43:01 -0500

    added cocktail images

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     cocktailForm.addEventListener('submit', (event) => {
         event.preventDefault();
         const cocktailName = document.getElementById('search').value
-        console.log(cocktailName)
+        // console.log(cocktailName)
 
         fetch(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
         .then(function (response) {
@@ -15,11 +15,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function renderDrinks(array) {
         array.forEach((eachDrink) => {
+            //Display Cocktail Name
             const cocktailList = document.getElementById('cocktail-list')
             const drinkNameLi = document.createElement('li')
             drinkNameLi.innerText = eachDrink.strDrink
             cocktailList.appendChild(drinkNameLi)
-
+            //Display Cocktail Image
+            const cocktailImg = document.createElement('img')
+            cocktailImg.src = eachDrink.strDrinkThumb
+            cocktailList.appendChild(cocktailImg)
         })
 
     }
