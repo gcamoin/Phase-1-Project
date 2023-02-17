43e61f7c380a6e5dda7916e1e263f0d00bd8ec91
Garrett <gcamoin5@gmail.com>
2023-02-17 09:24:43 -0500

    Created/Appended Cocktail Card

--- script.js
+++ script.js
@@ -15,15 +15,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function renderDrinks(array) {
         array.forEach((eachDrink) => {
-            //Display Cocktail Name
-            const cocktailList = document.getElementById('cocktail-list')
-            const drinkNameLi = document.createElement('li')
-            drinkNameLi.innerText = eachDrink.strDrink
-            cocktailList.appendChild(drinkNameLi)
-            //Display Cocktail Image
+            //Create Cocktail Card
+            const h2 = document.createElement('h2')
+            h2.innerText = eachDrink.strDrink
+            const cocktailCard = document.createElement('div')
             const cocktailImg = document.createElement('img')
             cocktailImg.src = eachDrink.strDrinkThumb
-            cocktailList.appendChild(cocktailImg)
+            const cardLikes = document.createElement('p')
+            const cardButton = document.createElement('button')
+            cardButton.textContent = 'Like'
+           //Append Elements to Cocktail Card
+           cocktailCard.append(h2, cocktailImg, cardButton, cardLikes)
+           const cocktailContainer = document.getElementById('cocktail-container') 
+           cocktailContainer.appendChild(cocktailCard)
+
         })
 
     }
